b2b47c441afa2c41707a62724d8f9327596e4e6b
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2020-05-21 17:29:04 +0500

    integrate notifications api with cms

--- colaraz/cms/static/js/header.js
+++ colaraz/cms/static/js/header.js
@@ -47,4 +47,76 @@ function openInviteFriendPopup(inviteFriendUrl) {
 
 $(document).ready(function (){
     makeAnimatedHeader();
+    getAndPopulateNotifications();
+    setInterval(getAndPopulateNotifications, 30000);
 });
+
+function getAndPopulateNotifications() {
+    $.ajax({
+        type: "GET",
+        url: colarazNotificationsFetchingUrl,
+        success: function (resp) {
+            let notifications = "";
+            let unreadNotificationsCount = 0;
+
+            resp.result.forEach(element => {
+                if (element.read == 0) newNotificationsCount += 1;
+                notifications += createNotification(element.image, element.description, element.time, element.read);
+            });
+
+            handleNotificationsListing(notifications);
+            handleNotificationsCount(unreadNotificationsCount);
+
+        },
+        error: function (errorMsg) {
+            console.error(`Notifications API gave following error: ${errorMsg}`);
+        },
+    });
+
+    function handleNotificationsListing(notifications) {
+        $("#notifications-list").html(notifications);
+    }
+    function handleNotificationsCount(count) {
+        if (count == 0) {
+            let countSpan = $("#unread-notifications-count");
+            countSpan.removeClass("count");
+            countSpan.html("");
+        } else {
+            let countSpan = $("#unread-notifications-count");
+            countSpan.addClass("count");
+            countSpan.html(count);
+        }
+    }
+
+    function createNotification(img_src, description, days_count, read) {
+        return `<li class="${read ? 'read' : 'unread'}" >` +
+            `<div class="media">` +
+            `<a href="#"><img src="${img_src}" alt=""></a>` +
+            `</div>` +
+            `<div class="description">` +
+            `${description}` +
+            `<p>${days_count}</p>` +
+            `</div>` +
+            `</li>`;
+    }
+}
+
+function markNotificationsAsRead() {
+    $.ajax({
+        type: "GET",
+        url: colarazNotificationsMarkingUrl,
+        success: function (resp) {
+        },
+        error: function (errorMsg) {
+        },
+    });
+}
+
+function removeNotificationsCount() {
+    let countSpan = $("#unread-notifications-count");
+    if (countSpan.html() !== "" || countSpan.hasClass("count")) {
+        countSpan.removeClass("count");
+        countSpan.html("");
+        markNotificationsAsRead();
+    }
+}
